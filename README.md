08fed829c33f8788acf21662715604ea8e2018d9
Karn Wong <karn@karnwong.me>
2025-08-06 16:57:05 +0700

    extract to auth.go

---
+++ README.md
@@ -0,0 +1,14 @@
+# gcal-tui
+
+Because I want a quick way to check today's events, or see my week's schedule.
+
+`iCalBuddy` is great, but then I don't use Apple Calendar. Also I use linux half the time.
+
+I found `Calcure` but I want a week-view.
+
+So here we are. Plus I use multiple calendars, so there's that.
+
+
+## Pre-reqs
+
+Create `Oauth Client ID` with scope `calendar.events`.
---
+++ internal/gcal/auth.go
@@ -0,0 +1,86 @@
+package gcal
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
+)
+
+func ReadOauthClientIDJSON() *oauth2.Config {
+	b, err := os.ReadFile("/Users/kahnwong/.config/gcal-tui/karnsireew.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	config, err := google.ConfigFromJSON(b, calendar.CalendarReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+
+	return config
+}
+
+func GetClient(config *oauth2.Config) *http.Client {
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Fatalf("Unable to close token file: %v", err)
+		}
+	}(f)
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Fatalf("Unable to cache oauth token: %v", err)
+		}
+	}(f)
+}
